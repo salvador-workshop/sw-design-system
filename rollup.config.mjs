09a136a12fa49118833308a26b6919e22cbee96a
RJ Salvador <salvador.workshop@gmail.com>
2024-05-18 15:01:35 -0400

    Added rollup to project

---
+++ rollup.config.mjs
@@ -0,0 +1,22 @@
+import resolve from '@rollup/plugin-node-resolve';
+import babel from '@rollup/plugin-babel';
+import mdx from '@mdx-js/rollup';
+import json from '@rollup/plugin-json';
+import commonjs from '@rollup/plugin-commonjs';
+
+export default {
+	input: 'src/index.js',
+	output: {
+		file: 'dist/sw-design-system.js',
+		format: 'esm'
+	},
+    plugins: [
+        resolve({
+            extensions: ['.mjs', '.js', '.json', '.node', '.jsx', '.md', '.mdx'],
+        }),
+        babel({ babelHelpers: 'bundled' }),
+        mdx(),
+        json(),
+        commonjs(),
+    ]
+};
