e39e0ce398ff04a138ed5d85072ec1a9d8cf7575
RJ Salvador <salvador.workshop@gmail.com>
2025-04-29 17:43:12 -0400

    we restarting out here

--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,115 @@
-// ...
+// Following the order from the docs (Apr 2025)
+
+//----------------
+// G E N E R A L
+
+export { default as Text } from './text'
+export { default as Button } from './button'
+export { default as Code } from './code'
+
+
+//--------------
+// L A Y O U T
+
+export { default as Grid } from './grid'
+export { default as Page } from './page'
+export { default as Spacer } from './spacer'
+
+
+//------------------
+// S U R F A C E S
+
+export { default as Card } from './card'
+export { default as Collapse } from './collapse'
+export { default as Fieldset } from './fieldset'
+
+
+//----------------------
+// D A T A   E N T R Y
+
+export { default as AutoComplete } from './auto-complete'
+export { default as ButtonGroup } from './button-group'
+export { default as Checkbox } from './checkbox'
+export { default as Input } from './input'
+export { default as Radio } from './radio'
+export { default as Select } from './select'
+export { default as Slider } from './slider'
+export { default as Textarea } from './textarea'
+export { default as Toggle } from './toggle'
+
+
+//--------------------------
+// D A T A   D I S P L A Y
+
+export { default as Avatar } from './avatar'
+export { default as Badge } from './badge'
+export { default as Capacity } from './capacity'
+export { default as Description } from './description'
+export { default as Display } from './display'
+export { default as Dot } from './dot'
+export { default as Tree } from './tree'
+export { default as Image } from './image'
+export { default as Keyboard } from './keyboard'
+export { default as Popover } from './popover'
+export { default as Table } from './table'
+export { default as Tag } from './tag'
+export { default as Tooltip } from './tooltip'
+export { default as User } from './user'
+
+
+//----------------------
+// F E E D B A C K
+
+export { default as Drawer } from './drawer'
+export { default as Loading } from './loading'
+export { default as Modal } from './modal'
+export { default as Note } from './note'
+export { default as Progress } from './progress'
+export { default as Rating } from './rating'
+export { default as Spinner } from './spinner'
+
+
+//----------------------
+// N A V I G A T I O N
+
+export { default as Breadcrumbs } from './breadcrumbs'
+export { default as Link } from './link'
+export { default as Pagination } from './pagination'
+export { default as Tabs } from './tabs'
+export { default as ButtonDropdown } from './button-dropdown'
+
+
+//----------------------
+// O T H E R S
+
+export { default as Divider } from './divider'
+export { default as Snippet } from './snippet'
+
+
+
+
+
+
+
+
+
+// export { default as GeistProvider } from './geist-provider'
+// export { default as Themes } from './themes'
+// export { default as useAllThemes } from './use-all-themes'
+// export { default as useToasts } from './use-toasts'
+// export { default as useBodyScroll } from './use-body-scroll'
+// export { default as useClipboard } from './use-clipboard'
+// export { default as useMediaQuery } from './use-media-query'
+// export { default as useKeyboard, KeyMod, KeyCode } from './use-keyboard'
+// export { default as useInput } from './use-input'
+// export { default as useModal } from './use-modal'
+// export { default as useTabs } from './use-tabs'
+// export { default as useClickAway } from './use-click-away'
+// export { default as useCurrentState } from './use-current-state'
+// export { default as CssBaseline } from './css-baseline'
+// export { default as useTheme } from './use-theme'
+// export { default as useClasses } from './use-classes'
+// export { default as useScale } from './use-scale'
+// export { withScale, ScalePropKeys, ScaleContext } from './use-scale'
+// export { useRect } from './utils/layouts'
+// export { default as Highlight } from './shared/highlight'
